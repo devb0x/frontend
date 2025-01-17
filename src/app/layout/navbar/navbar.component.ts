9494df02301810bd21dab00c193fdb95440a99a2
devb0x <78238534+devb0x@users.noreply.github.com>
2025-01-17 09:19:50 +0100

    change navbar logo and add scroll event for closing the mobile navbar

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router"
 import { CommonModule } from "@angular/common"
 
 import { Subscription } from "rxjs"
 
 import { AuthService } from "../../auth/auth.service"
+import { WINDOW, windowProvider } from "../../services/window.service"
 
 import { SearchBarComponent } from "../search-bar/search-bar.component"
 
@@ -19,17 +20,22 @@ import { SearchBarComponent } from "../search-bar/search-bar.component"
 		SearchBarComponent
 	],
 	templateUrl: './navbar.component.html',
-	styleUrl: './navbar.component.css'
+	styleUrl: './navbar.component.css',
+	providers: [
+		windowProvider
+	]
 })
 export class NavbarComponent {
 	userIsAuth:boolean = false
 	private authListenerSubs!: Subscription
 	private routerEventsSub!: Subscription
 	mobileMenuOpen: boolean = false
+	windowHeight = this.window.innerHeight
 
 	constructor(
 		private authService: AuthService,
-		private router: Router
+		private router: Router,
+		@Inject(WINDOW) private window: Window
 	) {}
 
 	ngOnInit() {
@@ -39,19 +45,29 @@ export class NavbarComponent {
 			.subscribe(isUserAuth => {
 				this.userIsAuth = isUserAuth
 			})
+		this.window.addEventListener('scroll', this.userScroll.bind(this))
+		console.log('Initial scrollY:', this.window.scrollY);
+		console.log('Window height:', this.windowHeight);
 	}
 
 	ngOnDestroy() {
-		this.authListenerSubs.unsubscribe()
-		this.routerEventsSub.unsubscribe()
+		if (this.authListenerSubs) {
+			this.authListenerSubs.unsubscribe()
+		}
+		if (this.routerEventsSub) {
+			this.routerEventsSub.unsubscribe()
+		}
+		this.window.removeEventListener('scroll', this.userScroll.bind(this))
 	}
 
 	toggleMobileNav() {
+		console.log('toggle mobile nav called')
 
 		this.mobileMenuOpen = !this.mobileMenuOpen;
 		if (!this.mobileMenuOpen) {
 			this.hiddeMobileNav()
 		}
+
 		setTimeout(() => {
 			const ul = document.querySelector('.mobile-navbar-list') as HTMLElement
 			ul.classList.toggle('translate')
@@ -67,6 +83,13 @@ export class NavbarComponent {
 		if (mobileNav) {
 			mobileNav.classList.remove('display')
 			mobileNav.classList.add('hidden')
+		}
+	}
+
+	userScroll() {
+		if (this.mobileMenuOpen && this.window.scrollY >= this.windowHeight) {
+			console.log('here you go')
+			this.toggleMobileNav()
 		}
 	}
 
